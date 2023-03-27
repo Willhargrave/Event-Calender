5f64e6542a9c2671f0bec8fd661043b8100f17b4
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2023-03-27 17:44:10 +0900

    gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_070441) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_071631) do
+  create_table "plans", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.date "date"
+    t.time "time"
+    t.text "description"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
+
+  add_foreign_key "plans", "users"
 end
