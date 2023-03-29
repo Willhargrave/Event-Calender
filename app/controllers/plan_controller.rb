dc79de51a578c071866c31f1759b695600184af9
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2023-03-29 18:49:48 +0900

    update create delete

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -1,2 +1,25 @@
 class PlanController < ApplicationController
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to :home
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @plan = Plan.find(params[:id])
+    if @plan.destroy
+      redirect_to :home
+    else
+      render :show, status: :unprocessible_entity
+    end
+  end
+
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  
 end
